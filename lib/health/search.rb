192585a8e2a91ec99a73a0c441de778b2a78c4d3
Jeff <malnick@gmail.com>
2014-11-29 13:49:44 -0800

    search

--- lib/health/search.rb
+++ lib/health/search.rb
@@ -7,6 +7,14 @@ module Health
 		end
 		
 		def get_result(query)
+			client = Twitter::Rest::Client.new do |config|
+				config.consumer_key        = "YOUR_CONSUMER_KEY"
+				config.consumer_secret     = "YOUR_CONSUMER_SECRET"
+				config.access_token        = "YOUR_ACCESS_TOKEN"
+				config.access_token_secret = "YOUR_ACCESS_SECRET"
+			end
+			
+			client.search	
 			query
 		end
 	end
