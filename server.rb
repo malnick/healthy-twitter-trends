4a6102ef856322b5f096ca8d2a9dbab6a5b6f5da
Jeff <malnick@gmail.com>
2014-12-24 17:42:12 -0800

    dir structure? h10 errors...

---
+++ server.rb
@@ -0,0 +1,46 @@
+require 'sinatra'
+require 'erb'
+require 'webrick'
+require 'logger'
+
+
+LOGFILE = File.expand_path(File.dirname(__FILE__)) + '/../logs/server.log'
+HTML	= File.expand_path(File.dirname(__FILE__)) + '/../public/index.html'
+D3	= File.expand_path(File.dirname(__FILE__)) + '/../public/d3fire.js'
+LOG = Logger.new(LOGFILE)
+
+opts = {
+	:Port		=> ENV['PORT'],
+	:Logger		=> WEBrick::Log::new(LOGFILE, WEBrick::Log::DEBUG),
+	:ServerType	=> WEBrick::Daemon,
+	:SSLEnable	=> false,
+}
+
+class Server < Sinatra::Base
+
+	get '/' do			
+		LOG.info("HTML Path: #{HTML}")
+		get_html
+	end
+
+	get '/d3fire.js' do
+		LOG.info("Sending d3fire.js")
+		LOG.info(D3)
+		content_type :js
+		send_file(D3)
+	end
+
+	get '/fuck' do
+		"fuck"
+	end
+
+
+	def get_html
+		File.open(HTML, 'r').readlines
+	end
+end		
+
+Rack::Handler::WEBrick.run(Server, opts) do |server|
+	[:INT, :TERM].each { |sig| trap(sig) { server.stop } }
+end
+
