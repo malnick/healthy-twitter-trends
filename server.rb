411d1cb4dcd7313fa888c1e143e903cf484b791e
Jeff <malnick@gmail.com>
2014-12-25 13:20:05 -0800

    local file paths for js

--- server.rb
+++ server.rb
@@ -3,37 +3,23 @@ require 'erb'
 require 'webrick'
 require 'logger'
 
-#LOGFILE = File.expand_path(File.dirname(__FILE__)) + '/logs/server.log'
+LOGFILE = File.expand_path(File.dirname(__FILE__)) + '/logs/server.log'
 HTML	= File.expand_path(File.dirname(__FILE__)) + '/public/index.html'
 D3	= File.expand_path(File.dirname(__FILE__)) + '/public/d3fire.js'
-LOG = Logger.new(STDOUT) #(LOGFILE)
+LOG = Logger.new(LOGFILE) #(LOGFILE)
 
 opts = {
 	:Port		=> ENV['PORT'],
-	:Logger		=> WEBrick::Log::new(STDOUT, WEBrick::Log::DEBUG),
-	#:ServerType	=> WEBrick::Daemon,
+	:Logger		=> WEBrick::Log::new(LOGFILE, WEBrick::Log::DEBUG),
 	:SSLEnable	=> true,
 }
 
 class Server < Sinatra::Base
 
-
 	get '/' do			
 		LOG.info("HTML Path: #{HTML}")
 		get_html
 	end
-
-	#get '/d3fire.js' do
-	#	LOG.info("Sending d3fire.js")
-	#	LOG.info(D3)
-	#	content_type :js
-	#	send_file(D3)
-	#end
-
-	get '/fuck' do
-		"fuck"
-	end
-
 
 	def get_html
 		File.open(HTML, 'r').readlines
