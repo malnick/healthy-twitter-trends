382a3543e1e9a69db885c46f36a36a9c475b64ea
Jeff <malnick@gmail.com>
2015-01-04 09:17:01 -0800

    emr working from cli

--- lib/twitq/emr.rb
+++ lib/twitq/emr.rb
@@ -5,16 +5,17 @@ require 'elasticity'
 module Twitq
 	class Emr
 		
-		def initialize(log, query)
-			@LOG = log
-			creds = set_creds
-			job = create_flow(query, creds)
+		def initialize(query)
+			@log = Logger.new(STDOUT)
+			set_creds
+			job = create_flow(query)
 			start_job(job, query)
 			@@results = get_results(query)
 		end
 
-		def create_flow(query, creds)
-			jobflow = Elasticity::JobFlow.new(creds['access_key_id'], creds['secret_access_key'])
+		def create_flow(query)
+			jobflow = Elasticity::JobFlow.new(@aws_config['access_key_id'], @aws_config['secret_access_key'])
+			@log.info("Adding wordcount step for #{query} query")
 			streaming_step = Elasticity::StreamingStep.new(
 				"s3n://twitq-#{query}/twitq-#{query}", # 's3n://elasticmapreduce/samples/wordcount/input', 
 				"s3n://twitq-#{query}/ouput", #'s3n://elasticityoutput/wordcount/output/2012-07-23', 
@@ -22,6 +23,7 @@ module Twitq
 				'aggregate')
 
 			jobflow.add_step(streaming_step)
+			jobflow
 		end
 
 		def start_job(job, query)
@@ -30,22 +32,19 @@ module Twitq
 			job.wait_for_completion do |elapsed_time, job_flow_status|
 				@log.info("Waiting for #{elapsed_time}, jobflow status: #{job_flow_status.state}")
 			end
+		end
 
 		def set_creds
-			aws_config = YAML.load_file(ENV['HOME'] + '/.aws.yaml') # ('~/.aws.yaml')
-			aws_config = {
-				:access_key_id 		=> aws_config['access_key_id'],
-				:secret_access_key 	=> aws_config['secret_access_key'],
-			}
+			@aws_config = YAML.load_file(ENV['HOME'] + '/.aws.yaml') # ('~/.aws.yaml')
+			puts @aws_config.inspect
 		end
 
 
 		def self.results
 			@@results
 		end
-
-
 	end
 end
 
+Twitq::Emr.new("judith")
 		
